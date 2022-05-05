582bc64ad1e63809c06a3bbe166e700ab19348f4
Wesley Ferreira <88842443+weeesferreira@users.noreply.github.com>
2022-05-04 22:49:28 -0300

    Add files via upload
    Atualização do trabalho (Maven)

---
+++ API_DOM_ROCK/src/main/java/Front.java
@@ -0,0 +1,17 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Front extends Application {
+
+	@Override
+	public void start(Stage cadastroStage) {
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
